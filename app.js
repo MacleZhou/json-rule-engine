eaabeba1596f45bd3203151340d4541d4602fe97
Raju Dhami <imRajuDhami@gmail.com>
2021-09-14 13:59:57 +0530

    added second rule data and rules

--- app.js
+++ app.js
@@ -1,44 +1,65 @@
 const express = require ( `express` ),
-			{ Engine } = require ( `json-rules-engine` ),
-			admission = require ( `./admission-rule.json` ),
+			{ v4: uuidv4 } = require ( `uuid` ),
 			app = express (),
 			port = 4400,
+			admission = require ( `./rules/admission.json` ),
+			{ Engine } = require ( `json-rules-engine` ),
 			engine = new Engine ();
+
+app.enable ( `trust proxy` );
+app.use ( express.json () );
+app.use ( express.urlencoded ( { extended : true } ) );
+
+app.post ( `/api/v1/admission`, ( req, res, next ) => {
+	const { body: { name, sslc, puc, graduation, niitinterview } } = req;
+
+	engine.addRule ( admission );
+
+	engine.run ( { sslc, puc, graduation, niitinterview } ).then ( ( conclusion ) => {
+		if ( conclusion.almanac.events.success.length > 0 ) {
+			if ( niitinterview === `true` ) {
+				return res.status ( 200 ).json ( {
+					apiVersion    : 1,
+					admissionData : {
+						name, sslc, puc, graduation, niitinterview,
+					},
+					response : {
+						type    : `Checking for Eligibility`,
+						message : `Congratulation, your eligible for Product Engineering Course.`,
+					},
+					timestamp : Date (),
+					studentId : uuidv4 (),
+				} );
+			}
+
+			return res.status ( 200 ).json ( {
+				apiVersion    : 1,
+				admissionData : {
+					name, sslc, puc, graduation, niitinterview,
+				},
+				response : {
+					type    : `Checking for Eligibility`,
+					message : `Congratulation, your eligible for Software Engineering Course.`,
+				},
+				timestamp : Date (),
+				studentId : uuidv4 (),
+			} );
+		}
+
+		return res.status ( 404 ).json ( {
+			apiVersion    : 1,
+			admissionData : {
+				name, sslc, puc, graduation, niitinterview,
+			},
+			response : {
+				type    : `Checking for Eligibility`,
+				message : `Sorry your not Eligible.`,
+			},
+			timestamp : Date (),
+		} );
+	} );
+} );
 
 app.listen ( port, () => {
 	console.log ( `JSON Rule Engine listening at http://localhost:${ port }` );
-} );
-
-engine.addRule ( admission );
-
-app.get ( `/v1/admission/:name/:board/:marks`, ( req, res ) => {
-	const { params: { name, board, marks } } = req;
-
-	engine.run ( { board, marks } ).then ( ( conclusion ) => {
-		conclusion.results.length > 0
-			? res.status ( 200 ).send ( {
-				apiVersion    : 1,
-				status        : 200,
-				admissionData : {
-					name, board, marks,
-				},
-				response  : conclusion?.events[ 0 ],
-				timestamp : Date (),
-			} )
-			: res.status ( 404 ).send ( {
-				apiVersion    : 1,
-				status        : 404,
-				admissionData : {
-					name, board, marks,
-				},
-				response : {
-					type   : `Checking for Eligibility`,
-					params : {
-						type   : `Admission`,
-						status : `Not Eligible`,
-					},
-				},
-				timestamp : Date (),
-			} );
-	} );
 } );
